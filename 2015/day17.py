bb29deba05aa887608c6c3fc09edc3e33003ca99
dan <dancrhorton@gmail.com>
2020-01-05 17:07:27 -0500

    day 17 2015

--- 2015/day17.py
+++ 2015/day17.py
@@ -2,7 +2,6 @@ import time
 from itertools import combinations
 
 from utils import open_file, get_group_size
-import numpy  as np
 
 day = 17
 year = 2015
@@ -15,7 +14,6 @@ def run1():
     target = 150
     string = open_file(day, year)
     containers = [int(c) for c in string]
-    max_c = target / min(containers)
     min_group_size = get_group_size(sorted(containers)[::-1], target)
     max_group_size = get_group_size(sorted(containers), target)
     combs = []
@@ -23,11 +21,11 @@ def run1():
         combs += [c for c in combinations(containers, i) if sum(c) == target]
     return len(combs)
 
+
 def run2():
     target = 150
     string = open_file(day, year)
     containers = [int(c) for c in string]
-    max_c = target / min(containers)
     min_group_size = get_group_size(sorted(containers)[::-1], target)
     combs = [c for c in combinations(containers, min_group_size) if sum(c) == target]
     return len(combs)
