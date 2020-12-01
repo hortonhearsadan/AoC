6de728ee0d4890ac7b9b20dc8a053554f87b9086
dan <dancrhorton@gmail.com>
2020-12-01 07:37:31 -0500

    2018

---
+++ template.py
@@ -0,0 +1,23 @@
+import time
+from utils import open_file
+day = 0
+year = 0
+
+TESTSTRING = ''''''
+STRING = ''''''
+
+def run1():
+    pass
+
+
+def run2():
+    pass
+
+
+if __name__ == "__main__":
+    a = time.time()
+    f = run1()
+    g = run2()
+    print(time.time() - a)
+    print(f"Part 1", f)
+    print(f"Part 2", g)
