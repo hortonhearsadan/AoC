c18f54ed19110f0e09d4a9eafa21c390c4888ee8
danielh <daniel.horton@broadsign.com>
2020-12-11 13:31:45 -0500

    day11

--- 2020/day11.py
+++ 2020/day11.py
@@ -26,35 +26,10 @@ def parse_input():
     return matrix
 
 
-def run1(plan):
-    plan = np.pad(plan, [(1, 1), (1, 1)], "constant", constant_values="F")
-    while True:
-        next_plan_full = []
-        next_plan_free = []
-
-        for row_idx, row in enumerate(plan[1:-1], 1):
-            for col_idx, seat in enumerate(row[1:-1], 1):
-                if seat == ".":
-                    continue
-                adj = plan[row_idx - 1 : row_idx + 2, col_idx - 1 : col_idx + 2]
-                occupied = np.count_nonzero(adj == "#")
-
-                if seat == "L":
-                    if occupied == 0:
-                        next_plan_full.append((row_idx, col_idx))
-
-                elif seat == "#":
-                    if occupied >= 5:
-                        next_plan_free.append((row_idx, col_idx))
-
-        if next_plan_free or next_plan_full:
-            for y,x in next_plan_free:
-                plan[y,x] = 'L'
-            for y, x in next_plan_full:
-                plan[y, x] = "#"
-
-        else:
-            return np.count_nonzero(plan == "#")
+def get_occupied_adj(plan, r, c):
+    adj = plan[r - 1 : r + 2, c - 1 : c + 2]
+    occupied = np.count_nonzero(adj == "#")
+    return occupied
 
 
 def look(x):
@@ -66,8 +41,7 @@ def look(x):
         return True, False
 
 
-
-def count_occupied(plan, row, col):
+def get_occupied_sight(plan, row, col):
     h, w = plan.shape
     left = plan[row, :col][::-1]
     right = plan[row, col + 1:]
@@ -75,49 +49,50 @@ def count_occupied(plan, row, col):
     down = plan[row + 1:, col]
     count = 0
 
+    for sight in [left, right, up, down]:
+        for seat in sight:
+            stop, add = look(seat)
+            if stop:
+                if add:
+                    count += 1
+                break
+
     for i in range(1, w):
         x = plan[row - i, col - i]
-        stop,add = look(x)
+        stop, add = look(x)
         if stop:
             if add:
-                count+=1
+                count += 1
             break
 
     for i in range(1, w):
         x = plan[row + i, col + i]
-        stop,add = look(x)
+        stop, add = look(x)
         if stop:
             if add:
-                count+=1
+                count += 1
             break
 
     for i in range(1, w):
         x = plan[row + i, col - i]
-        stop,add = look(x)
+        stop, add = look(x)
         if stop:
             if add:
-                count+=1
+                count += 1
             break
 
     for i in range(1, w):
         x = plan[row - i, col + i]
-        stop,add = look(x)
+        stop, add = look(x)
         if stop:
             if add:
-                count+=1
+                count += 1
             break
 
-    for sight in [left, right, up, down]:
-        for seat in sight:
-            stop,add = look(seat)
-            if stop:
-                if add:
-                    count+=1
-                break
     return count
 
 
-def run2(plan):
+def run1(plan, occ_fnc):
     plan = np.pad(plan, [(1, 1), (1, 1)], "constant", constant_values="F")
     while True:
         next_plan_full = []
@@ -127,7 +102,7 @@ def run2(plan):
             for col_idx, seat in enumerate(row[1:-1], 1):
                 if seat == ".":
                     continue
-                occupied = count_occupied(plan, row_idx, col_idx)
+                occupied = occ_fnc(plan, row_idx, col_idx)
 
                 if seat == "L":
                     if occupied == 0:
@@ -150,8 +125,8 @@ def run2(plan):
 if __name__ == "__main__":
     a = time.time()
     inputs = parse_input()
-    f = run1(inputs)
-    g = run2(inputs)
+    f = run1(inputs, get_occupied_adj)
+    g = run1(inputs, get_occupied_sight)
     print(f"Part 1: {f}")
     print(f"Part 2: {g}")
     print(f"Runtime: {round((time.time() - a)*1000,3)}ms")
